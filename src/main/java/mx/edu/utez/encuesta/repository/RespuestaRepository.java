40bb222ba55aa5bf8a54fe0304361131cf27c75a
GABY <GABY@GABY-PC>
2018-02-19 23:47:09 -0600

    prueba pull

---
+++ src/main/java/mx/edu/utez/encuesta/repository/RespuestaRepository.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.edu.utez.encuesta.repository;
+
+import mx.edu.utez.encuesta.entity.Respuesta;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author dvd
+ */
+@Configuration
+@Repository
+public interface RespuestaRepository extends JpaRepository<Respuesta, Integer> {
+    Respuesta findById(Integer idRespuesta);
+    
+}
